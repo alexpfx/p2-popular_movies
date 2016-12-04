fb8a107d809b71078f771320aa87f8c09d953588
Alexandre Alessi <alexpfba@gmail.com>
2016-12-04 20:40:53 -0200

    content provider

--- app/src/main/java/udacity/nanodegree/android/p2/database/MoviesContentProvider.java
+++ app/src/main/java/udacity/nanodegree/android/p2/database/MoviesContentProvider.java
@@ -2,7 +2,10 @@ package udacity.nanodegree.android.p2.database;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.Nullable;
@@ -12,32 +15,86 @@ import android.support.annotation.Nullable;
  */
 
 public class MoviesContentProvider extends ContentProvider {
+    public static final int MOVIE = 300;
     private MoviesOpenHelper moviesOpenHelper;
+
+    private static final UriMatcher uriMatcher = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MoviesContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, MoviesContract.PATH_MOVIE, MOVIE);
+
+        return matcher;
+    }
 
     @Override
     public boolean onCreate() {
         moviesOpenHelper = MoviesOpenHelper.getInstance(getContext());
-        return false;
+        return true;
+    }
+
+    public static final SQLiteQueryBuilder queryBuilder;
+
+    static {
+        queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(MoviesContract.MovieEntry.TABLE_NAME);
     }
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
-
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getType(Uri uri) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor cursor;
+        switch (uriMatcher.match(uri)) {
+            case MOVIE:
+                cursor = moviesOpenHelper.getReadableDatabase()
+                        .query(MoviesContract.MovieEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("url doesn't match: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        SQLiteDatabase database = moviesOpenHelper.getWritableDatabase();
+
+        Uri returnUri;
+
+        switch (uriMatcher.match(uri)) {
+            case MOVIE: {
+                long id = database.insert(MoviesContract.MovieEntry.TABLE_NAME, null, contentValues);
+                if (id > 0) {
+                    returnUri = MoviesContract.MovieEntry.buildMovieUri(id);
+                } else {
+                    throw new SQLException("failed to insert row into " + uri);
+                }
+            }
+            break;
+            default:
+                throw new UnsupportedOperationException("uri doesn't match: " + uri);
+        }
+
+        getContext().getContentResolver()
+                .notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = uriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIE:
+                return MoviesContract.MovieEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknow uri " + uri);
+        }
     }
 
     @Override
--- app/src/main/java/udacity/nanodegree/android/p2/database/MoviesContract.java
+++ app/src/main/java/udacity/nanodegree/android/p2/database/MoviesContract.java
@@ -1,5 +1,8 @@
 package udacity.nanodegree.android.p2.database;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,8 +10,21 @@ import android.provider.BaseColumns;
  */
 
 public class MoviesContract {
+    public static final String CONTENT_AUTHORITY = "udacity.nanodegree.android.p2";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = "movie";
 
     public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_MOVIE)
+                .build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_MOVIE;
+
         public static final String TABLE_NAME = "MOVIES";
 
         public static final String COLUMN_TITLE = "TITLE";
@@ -20,6 +36,12 @@ public class MoviesContract {
         public static final String COLUMN_USER_RATING = "RATING";
 
         public static final String COLUMN_RELEASE = "RELEASE_DATE";
+
+        public static final Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
 
     }
 

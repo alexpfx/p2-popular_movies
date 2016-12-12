ebcf95eb7eeca0c01ce42fc1dfb606e97f3985a9
Alexandre Alessi <alexpfba@gmail.com>
2016-12-11 23:36:53 -0200

    logica para persistir quando houver clique no botao favorito

--- app/src/main/java/udacity/nanodegree/android/p2/MainActivity.java
+++ app/src/main/java/udacity/nanodegree/android/p2/MainActivity.java
@@ -1,25 +1,23 @@
 package udacity.nanodegree.android.p2;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import udacity.nanodegree.android.p2.database.MoviesContentProvider;
-import udacity.nanodegree.android.p2.database.MoviesContract;
-import udacity.nanodegree.android.p2.database.MoviesOpenHelper;
 import udacity.nanodegree.android.p2.model.comum.MovieViewModel;
 import udacity.nanodegree.android.p2.model.detail.DetailFragment;
 import udacity.nanodegree.android.p2.model.detail.DetailHandler;
 import udacity.nanodegree.android.p2.model.detail.TrailerHandler;
 import udacity.nanodegree.android.p2.model.movie.MoviesFragment;
 
-import static udacity.nanodegree.android.p2.database.MoviesContract.*;
+import static udacity.nanodegree.android.p2.database.MoviesContract.MovieEntry;
 
 public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener, DetailHandler.DetailHandlerDelegate, TrailerHandler.TrailerHandlerDelegate {
 
     private static final String TAG = "MainActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,17 +54,60 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
 
     @Override
     public void onFavorite(boolean isFavorited, MovieViewModel viewModel) {
-        if (isFavorited){
-
-//            getContentResolver().query(MovieEntry.CONTENT_URI, new String[]{MovieEntry._ID}, MovieEntry.COLUMN_POSTER)
-
+        if (isFavorited) {
+            insertOrUpdate(viewModel);
+        } else {
+            deleteIfExists(viewModel);
         }
+    }
 
+    private void deleteIfExists(MovieViewModel viewModel) {
+        if (movieExists(viewModel)) {
+            deleteMovie(viewModel);
+        }
+    }
+
+    private void deleteMovie(MovieViewModel viewModel) {
+        getContentResolver().delete(MovieEntry.CONTENT_URI, MovieEntry.COLUMN_MOVIE_ID + "= ?", new String[]{String.valueOf(viewModel.getId())});
+    }
+
+    private void insertOrUpdate(MovieViewModel viewModel) {
+        if (movieExists(viewModel)) {
+            insertMovie(viewModel);
+        } else {
+            updateMovie(viewModel);
+        }
+    }
+
+    private void updateMovie(MovieViewModel viewModel) {
+
+    }
+
+    private boolean movieExists(MovieViewModel viewModel) {
+        Cursor cursor = getContentResolver().query(MovieEntry.CONTENT_URI, new String[]{MovieEntry._ID}, MovieEntry.COLUMN_MOVIE_ID + "= ?", new String[]{String.valueOf(viewModel.getId())}, null);
+        return cursor.moveToFirst();
+    }
+
+    private void insertMovie(MovieViewModel viewModel) {
+        getContentResolver().insert(MovieEntry.CONTENT_URI, createContentValues(viewModel));
+    }
+
+    private ContentValues createContentValues(MovieViewModel viewModel) {
+        ContentValues c = new ContentValues();
+        c.put(MovieEntry.COLUMN_MOVIE_ID, viewModel.getId());
+        c.put(MovieEntry.COLUMN_POSTER, viewModel.getPosterImage());
+        c.put(MovieEntry.COLUMN_RELEASE, viewModel.getReleaseDate()
+                .getTime());
+        c.put(MovieEntry.COLUMN_SYNOPSIS, viewModel.getSynopsys());
+        c.put(MovieEntry.COLUMN_TITLE, viewModel.getTitle());
+        c.put(MovieEntry.COLUMN_USER_RATING, viewModel.getVoteAvg());
+        //runtime
+        return c;
     }
 
     @Override
     public void onTrailerPlay(String key) {
-        Log.d(TAG, "onTrailerPlay: "+key);
+        Log.d(TAG, "onTrailerPlay: " + key);
         //        String key = vm.getKey();
 //        Context context = view.getContext();
 //        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(context.getString(R.string.youtube_app, key)));

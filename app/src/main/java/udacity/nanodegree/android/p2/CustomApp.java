4e8bf88056f7dd48f84a0973ed629487d40505cf
Alexandre Alessi <alexpfba@gmail.com>
2016-12-11 22:49:44 -0200

    aplicando dbviewer

---
+++ app/src/main/java/udacity/nanodegree/android/p2/CustomApp.java
@@ -0,0 +1,18 @@
+package udacity.nanodegree.android.p2;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.clough.android.androiddbviewer.ADBVApplication;
+
+import udacity.nanodegree.android.p2.database.MoviesOpenHelper;
+
+/**
+ * Created by alexandre on 11/12/2016.
+ */
+
+public class CustomApp extends ADBVApplication{
+    @Override
+    public SQLiteOpenHelper getDataBase() {
+        return new MoviesOpenHelper(getApplicationContext());
+    }
+}
